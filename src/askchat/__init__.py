574a9f352a1e124a4fe561e3a0804110075d34fe
rex <1073853456@qq.com>
2026-02-21 02:08:16 +0800

    update version

--- src/askchat/__init__.py
+++ src/askchat/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Rex Wang"""
 __email__ = '1073853456@qq.com'
-__version__ = '2.0.1'
+__version__ = '2.1.0'
 
 from .elements import ChatFileCompletionType, EnvNameCompletionType
 from .utils import show_resp, set_keys, initialize_config, write_config
